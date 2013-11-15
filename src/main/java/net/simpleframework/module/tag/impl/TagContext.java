dc077e3accc4e956402a3346e0c44136b8cd2e9e
simpleframework <cknet@126.com>
2013-11-15 11:37:09 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/module/tag/impl/TagContext.java
+++ src/main/java/net/simpleframework/module/tag/impl/TagContext.java
@@ -14,7 +14,7 @@ import net.simpleframework.module.tag.TagSynonyms;
  * Licensed under the Apache License, Version 2.0
  * 
  * @author 陈侃(cknet@126.com, 13910090885)
- *         http://code.google.com/p/simpleframework/
+ *         https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
 public abstract class TagContext extends AbstractDbModuleContext implements ITagContext {
